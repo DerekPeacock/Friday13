62e3498182913bf4ca887586e37b6bd81b583908
Derek Peacock <derek_peacock@yahoo.com>
2022-10-21 13:21:24 +0100

    Added music list

--- src/Week5/MusicList.java
+++ src/Week5/MusicList.java
@@ -1,21 +1,19 @@
-import helpers.ConsoleColours;
-import Week3.Week03;
-public class App 
+package Week5;
+import helpers.*;
+
+public class MusicList 
 {
-    public static void main(String[] args) throws Exception 
+    public static void main(String[] args)
     {
-        //System.out.println(ConsoleColours.ANSI_BRIGHT_BG_BLUE);
         System.out.println(ConsoleColours.CONSOLE_CLEAR);
 
         System.out.println("==================================");
         System.out.println("CO452 Programming Concepts 2022/23");
         System.out.println("==================================");
         System.out.println();
-        System.out.println(" by Nicholas Day and Derek Peacock");
+        System.out.println("  CW1 Course Work Music List ");
         System.out.println();
-
-        Week03 app = new Week03();
-        app.run();
-
-    }
+        System.out.println(" by Student Name");
+        System.out.println();
+    }      
 }
